30f065fb7457bb5e4c61b53115b4e67166b3438a
sagamantus <74814583+sagamantus@users.noreply.github.com>
2022-06-19 23:20:15 +0530

    add input and increment increment function

--- main.c
+++ main.c
@@ -1,53 +1,74 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include "dll.c"
-#include"operations.c"
+/* Including header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include "operations.c"
 
 int main()
 {
 	/* Declare the pointers */
 	dll *head1 = NULL, *tail1 = NULL, *head2 = NULL, *tail2 = NULL, *headR = NULL, *tailR = NULL;
-	char option, operator;
-	char c;
+	char option, operator, c;
+	int flag = 0;
 	do
 	{
 		/* Code for reading the inputs */
-		
+		printf("Enter the number: ");
+		while (1)
+		{
+			scanf("%c", &c);
+			if (c == '\n')
+				break;
+			if (c == '-')
+				flag++;
+			else
+			{
+				if (flag == 1)
+				{
+					push_back(&head1, &tail1, -(((int)c) - 48));
+					flag++;
+				}
+				else
+					push_back(&head1, &tail1, ((int)c) - 48);
+			}
+		}
+		parse_int(&head1, &tail1);
+
 		/* Function for extracting the operator */
 		printf("Enter the operation to perform:\n");
-		printf("---------------------------------------------------");
-		printf("Enter i for increment operaion.\n ");
+		printf("---------------------------------------------------\n");
+		printf("Enter i for increment operaion.\n");
 		printf("Enter d for decrement operaion.\n");
 		printf("Enter + for addition operaion.\n");
-		printf("Enter - for substraction operaion.\n ");
+		printf("Enter - for substraction operaion.\n");
 		printf("Enter * for multiplication operaion.\n");
-		printf("Enter / for division operaion.\n ");
-		scanf(" %c",&operator);
+		printf("Enter / for division operaion.\n");
+		scanf(" %c", &operator);
 
 		switch (operator)
 		{
-			case 'i':
-				increment();
-			case 'd':
-				decrement();
-			case '+':
-				/* call the function to perform the addition operation */
-				break;
-			case '-':	
-				/* call the function to perform the subtraction operation */
-				break;
-			case '*':	
-				/* call the function to perform the multiplication operation */
-				break;
-			case '/':	
-				/* call the function to perform the division operation */
-				break;
-			default:
-				printf("Invalid Input:-( Try again...\n");
+		case 'i':
+			increment(&head1, &tail1);
+			print_front(&head1, &tail1);
+		case 'd':
+			//(&head1, &tail1);
+		case '+':
+			/* call the function to perform the addition operation */
+			break;
+		case '-':
+			/* call the function to perform the subtraction operation */
+			break;
+		case '*':
+			/* call the function to perform the multiplication operation */
+			break;
+		case '/':
+			/* call the function to perform the division operation */
+			break;
+		default:
+			printf("Invalid Input:-( Try again...\n");
 		}
 		printf("Want to continue? Press [yY | nN]: ");
 		scanf(" %c", &option);
-	}while (option == 'y' || option == 'Y');
+	} while (option == 'y' || option == 'Y');
 
 	return 0;
 }
--- operations.c
+++ operations.c
@@ -1,22 +1,67 @@
 /* Including header files */
 #include <stdio.h>
 #include <stdlib.h>
+#include "dll.c"
+
 #define SUCCESS GRN
 #define FAILURE YEL
-#include "dll.c"
+
+int increment(dll **head, dll **tail)
+{
+    dll *temp = *tail;
+    int carry = 1;
+    if (*head == NULL)
+    {
+        fprintf(stderr, RED "[ERROR]" NC ": Overflow! \n");
+        return EXIT_FAILURE;
+    }
+    if ((*head)->data >= 0)
+    {
+        while (temp != NULL)
+        {
+            if (carry == 0)
+                return EXIT_SUCCESS;
+            int digit = temp->data + carry;
+            temp->data = digit % 10;
+            carry = digit / 10;
+            temp = temp->prev;
+        }
+        if (carry)
+            push_front(head, tail, carry);
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        while( temp != NULL)
+        {
+            if (temp->data <0)
+                temp->data++;
+            else
+            {
+                if (temp->data == 0)
+                    if (temp->prev->data == -1) temp->data = -9;
+                    else temp->data = 9;
+                else
+                {
+                    temp->data--;
+                    break;
+                }
+            }
+            temp = temp->prev;
+        }
+        parse_int(head, tail);
+    }
+}
 
 int addition(dll **head1, dll **tail1, dll **head2, dll **tail2, dll **headR, dll **tailR)
 {
     /* Definition goes here */
-
 }
 
 int substraction(dll **head1, dll **tail1, dll **head2, dll **tail2, dll **headR, dll **tailR)
 {
     /* Definition goes here */
-    int borrow = 0 ;
-
-
+    int borrow = 0;
 }
 
 int multiplication(dll **head1, dll **tail1, dll **head2, dll **tail2, dll **headR, dll **tailR)
